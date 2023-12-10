b2903eeedf72f426ad5ccf9ffd2ad37fcb6e5ab0
Maria Jose Veloso <majitovelosoojeda@gmail.com>
2023-12-10 18:05:38 -0300

    Se sube  el modulo de administracion de las obras del sitio

---
+++ js/producto_editar.js
@@ -0,0 +1,64 @@
+console.log(location.search)     // lee los argumentos pasados a este formulario
+var id=location.search.substr(4)  // producto_update.html?id=1
+console.log(id)
+const { createApp } = Vue
+  createApp({
+    data() {
+      return {
+        id:0,
+        titulo:"",
+        autor:"",
+        descripcion:"",
+        tipo:"",
+        imagen:"",
+        url:'http://noratr.pythonanywhere.com/obras/'+id,
+       }  
+    },
+    methods: {
+        fetchData(url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    this.id=data.id
+                    this.titulo = data.titulo;
+                    this.imagen=data.imagen
+                    this.autor=data.autor
+                    this.descripcion=data.descripcion
+                    this.tipo=data.tipo                
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.error=true              
+                })
+        },
+        modificar() {
+            let producto = {
+                titulo:this.titulo,
+                imagen:this.imagen,
+                autor:this.autor,
+                descripcion:this.descripcion,
+                tipo:this.tipo
+                
+            }
+            var options = {
+                body: JSON.stringify(producto),
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                redirect: 'follow'
+            }
+            fetch(this.url, options)
+                .then(function () {
+                    alert("Registro modificado")
+                    window.location.href = "productos.html"; // navega a productos.html          
+                })
+                .catch(err => {
+                    console.error(err);
+                    alert("Error al Modificar")
+                })      
+        }
+    },
+    created() {
+        this.fetchData(this.url)
+    },
+  }).mount('#app')
---
+++ js/productos.js
@@ -0,0 +1,76 @@
+const { createApp } = Vue
+  createApp({
+    data() {
+      return {
+        productos:[],
+        producto:{},
+        id:0,
+        titulo:"",
+        autor:"",
+        tipo:"",
+        descripcion:"",
+        imagen:"",
+        
+ 
+        url:'http://noratr.pythonanywhere.com/obras',   // si ya lo subieron a pythonanywhere
+        error:false,
+        cargando:true,
+
+    }
+    },
+    methods: {
+        fetchData(url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    this.productos = data;
+                    this.cargando=false
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.error=true              
+                })
+        },
+        eliminar(id) {
+            const url = this.url+'/' + id;
+            var options = {
+                method: 'DELETE',
+            }
+            fetch(url, options)
+                .then(res => res.text()) // or res.json()
+                .then(res => {
+			 alert('Registro Eliminado')
+                    location.reload(); // recarga el json luego de eliminado el registro
+                })
+        },
+        grabar(){
+            let producto = {
+                titulo:this.titulo,
+                autor: this.autor,
+                tipo:this.tipo,
+                descripcion: this.descripcion,
+                imagen:this.imagen,
+            }
+            console.log(producto)
+            var options = {
+                body:JSON.stringify(producto),
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                redirect: 'follow'
+            }
+            fetch(this.url, options)
+                .then(function () {
+                    alert("Registro grabado")
+                    window.location.href = "./productos.html";  // recarga productos.html
+                })
+                .catch(err => {
+                    console.error(err);
+                    alert("Error al Grabar")  // puedo mostrar el error tambien
+                })      
+        }
+    },
+    created() {
+        this.fetchData(this.url)
+    },
+  }).mount('#app')
+            
